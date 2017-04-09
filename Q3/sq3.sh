c4dd89d5d27d2c84880a01576fc0f3b14762840c
roledenez <sroledenez@gmail.com>
2017-04-09 04:36:31 +0000

    completed the Q3 sql part

--- Q3/sq3.sh
+++ Q3/sq3.sh
@@ -4,6 +4,7 @@
 
 python3 csv2sqlite.py --table-name biopics --input biopics.csv --output biopics.sqlite
 
-sqlite3 biopics.sqlite 'select subject_sex,sum(box_office)  from biopics group by subject_sex' > result
-awk -F ',' 'BEGIN{print "<table>"} {print "<tr><th>Gender</th><th>Total Amount</th></tr><tr>";for(i=1;i<=NF;i++)print "<td>" $i"</td>";print "</tr>"} END{print "</table>"}' result
+sqlite3 biopics.sqlite 'select year_release,sum(box_office)  from biopics group by year_release' > result
+
+awk -F ',' 'BEGIN{print "<table><tr><th>Gender</th><th>Total Amount</th></tr>"} {print "<tr>";for(i=1;i<=NF;i++)print "<td>" $i"</td>";print "</tr>"} END{print "</table>"}' result > sa3.html
 rm result
