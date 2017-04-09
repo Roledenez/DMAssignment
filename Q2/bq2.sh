ea1cec48c45d5ce7231f46853527a22b7b3c1875
roledenez <sroledenez@gmail.com>
2017-04-09 03:29:34 +0000

    clearn the q2 shell commnds

--- Q2/bq2.sh
+++ Q2/bq2.sh
@@ -1,8 +1,9 @@
 ./get_data.sh
-./clean.sh
+#./clean.sh
 awk -F ',' '{print $13 "," $5}' biopics.csv > tmp
 grep -e "Male,[0-9]" tmp > male
 grep -e "Female,[0-9]" tmp > female
 awk -F ',' '{sum+=$2} END{print "Male " sum "$"}' male >> result
 awk -F ',' '{sum+=$2} END{print "Female " sum "$"}' female >> result
 awk 'BEGIN{print "<table>"} {print "<tr><th>Gender</th><th>Total Amount</th></tr><tr>";for(i=1;i<=NF;i++)print "<td>" $i"</td>";print "</tr>"} END{print "</table>"}' result
+rm biopics.csv female male result tmp
--- Q2/t.sh
+++ Q2/t.sh
@@ -3,4 +3,4 @@ grep -e "Male,[0-9]" tmp > male
 grep -e "Female,[0-9]" tmp > female
 awk -F ',' '{sum+=$2} END{print "Male " sum "$"}' male >> result
 awk -F ',' '{sum+=$2} END{print "Female " sum "$"}' female >> result
-awk 'BEGIN{print "<table>"} {print "<tr><th>Gender</th><th>Total Amount</th></tr><tr>";for(i=1;i<=NF;i++)print "<td>" $i"</td>";print "</tr>"} END{print "</table>"}' result
+awk 'BEGIN{print "<table><tr><th>Gender</th><th>Total Amount</th></tr>"} {print "<tr>";for(i=1;i<=NF;i++)print "<td>" $i"</td>";print "</tr>"} END{print "</table>"}' result
