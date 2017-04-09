3c1a1a4e26f9b97de46e7529afa100594e54c8e2
roledenez <sroledenez@gmail.com>
2017-04-09 10:37:49 +0000

    fixed the Q2 html file type issue

---
+++ Q1.new/sq1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+wget https://goo.gl/BhphrS
+mv BhphrS biopics.csv
+awk -F ',' '{gsub("-","NO",$5);print $5}' biopics.csv
+awk -F ',' '{if (!$11) {print $11, "NO"} else {print $11}}' biopics.csv
+python3 csv2sqlite.py --table-name biopics --input biopics.csv --output biopics.sqlite
+
+sqlite3 biopics.sqlite 'select director, count(director) from biopics group by director order by director asc;' > sa1.txt
--- Q2/sq2.sh
+++ Q2/sq2.sh
@@ -5,5 +5,5 @@
 python3 csv2sqlite.py --table-name biopics --input biopics.csv --output biopics.sqlite
 
 sqlite3 biopics.sqlite 'select subject_sex,sum(box_office)  from biopics group by subject_sex' > result
-awk -F ',' 'BEGIN{print "<table>"<tr><th>Gender</th><th>Total Amount</th></tr>"} {print "<tr>";for(i=1;i<=NF;i++)print "<td>" $i"</td>";print "</tr>"} END{print "</table>"}' result > sa2.html
+awk -F ',' 'BEGIN{print "<table><tr><th>Gender</th><th>Total Amount</th></tr>"} {print "<tr>";for(i=1;i<=NF;i++)print "<td>" $i"</td>";print "</tr>"} END{print "</table>"}' result > sa2.html
 rm result
