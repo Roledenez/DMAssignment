842dc6a07e2f883195673fed03c5d6603908c900
roledenez <sroledenez@gmail.com>
2017-04-09 03:55:15 +0000

    fixed the bug creating ba2.html

--- Q2/bq2.sh
+++ Q2/bq2.sh
@@ -5,5 +5,5 @@ grep -e "Male,[0-9]" tmp > male
 grep -e "Female,[0-9]" tmp > female
 awk -F ',' '{sum+=$2} END{print "Male " sum "$"}' male >> result
 awk -F ',' '{sum+=$2} END{print "Female " sum "$"}' female >> result
-awk 'BEGIN{print "<table>"} {print "<tr><th>Gender</th><th>Total Amount</th></tr><tr>";for(i=1;i<=NF;i++)print "<td>" $i"</td>";print "</tr>"} END{print "</table>"}' result
+awk 'BEGIN{print "<html><table>"} {print "<tr><th>Gender</th><th>Total Amount</th></tr><tr>";for(i=1;i<=NF;i++)print "<td>" $i"</td>";print "</tr>"} END{print "</table></html>"}' result > ba2.html
 rm biopics.csv female male result tmp
