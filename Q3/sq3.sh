bd4f7cf1af410e613fc8b7470afeaf5e6a30b580
roledenez <sroledenez@gmail.com>
2017-04-09 04:56:11 +0000

    completed the assignment

--- Q3/sq3.sh
+++ Q3/sq3.sh
@@ -4,7 +4,7 @@
 
 python3 csv2sqlite.py --table-name biopics --input biopics.csv --output biopics.sqlite
 
-sqlite3 biopics.sqlite 'select year_release,sum(box_office)  from biopics group by year_release' > result
+sqlite3 biopics.sqlite 'select year_release,avg(box_office)  from biopics group by year_release' > result
 
-awk -F ',' 'BEGIN{print "<table><tr><th>Gender</th><th>Total Amount</th></tr>"} {print "<tr>";for(i=1;i<=NF;i++)print "<td>" $i"</td>";print "</tr>"} END{print "</table>"}' result > sa3.html
+awk -F ',' 'BEGIN{print "<table><tr><th>Year</th><th>Average Groess</th></tr>"} {print "<tr>";for(i=1;i<=NF;i++)print "<td>" $i"</td>";print "</tr>"} END{print "</table>"}' result > sa3.html
 rm result
