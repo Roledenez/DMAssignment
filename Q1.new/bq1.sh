399b1d82d40245b1f04d16fb27eaee60766e0401
roledenez <sroledenez@gmail.com>
2017-04-09 11:20:30 +0000

    fixed the issue with Q1/sa1.txt

--- Q1.new/bq1.sh
+++ Q1.new/bq1.sh
@@ -2,4 +2,4 @@
 
 ./get_data.sh
 ./clean.sh
-awk 'BEGIN { FS=OFS=SUBSEP=","}{arr[$6]++ }END {for (i in arr) print i,arr[i]}' biopics.csv | sort > ba1.txt
+awk 'BEGIN { FS=OFS=SUBSEP=","}{arr[$6]+=$7 }END {for (i in arr) print i,arr[i]}' biopics.csv | sort > ba1.txt
--- Q1.new/sq1.sh
+++ Q1.new/sq1.sh
@@ -5,4 +5,4 @@ awk -F ',' '{gsub("-","NO",$5);print $5}' biopics.csv
 awk -F ',' '{if (!$11) {print $11, "NO"} else {print $11}}' biopics.csv
 python3 csv2sqlite.py --table-name biopics --input biopics.csv --output biopics.sqlite
 
-sqlite3 biopics.sqlite 'select director, count(director) from biopics group by director order by director asc;' > sa1.txt
+sqlite3 biopics.sqlite 'select director, sum(number_of_subjects) from biopics group by director order by director asc;' > sa1.txt
